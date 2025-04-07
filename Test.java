5ccf663fc8bde3acc9892b61376728212d72600b
MADHANKUMAR S <138836953+madhankumar-2131@users.noreply.github.com>
2025-04-07 13:22:47 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,12 +1,7 @@
-public class Test {
+public class Test{
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Please provide two numbers as arguments.");
-            return;
-        }
-
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        System.out.println("Sum = " + (a + b));
+        System.out.println("****");
+        System.out.println("Hello Madhan");
+        System.out.println("****");
     }
 }
