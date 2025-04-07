0159ec63e398a776caf1943dd620600fb2955a44
MADHANKUMAR S <138836953+madhankumar-2131@users.noreply.github.com>
2025-04-07 13:15:52 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,11 @@
+import java.util.*;
 public class  Test{
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
         System.out.println("****");
         System.out.println("Hello Madhan");
         System.out.println("****");
+        System.out.println(a);
     }
 }
